800f48df4f9f9db3be36d4f79fa32f1450e903e4
Lona-up <juntai772@gmail.com>
2026-04-16 16:15:54 +0900

    パスの修正

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: {
     index: 'src/index.ts',
     react: 'src/react.ts',
-    'core/worker': 'src/core/worker.ts',
+    worker: 'src/core/worker.ts',
   },
   format: ['esm', 'cjs'],
   dts: true,
